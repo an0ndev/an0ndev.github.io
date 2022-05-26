99e06eb6cbfad9c7e1f67b29585a0fc2a7d4393b
Eric Reed <mrericcoolnessreed@gmail.com>
2022-05-26 12:21:54 -0400

    brightness mode switcher

--- res/script.js
+++ res/script.js
@@ -1,4 +1,23 @@
+let initialBrightness = true;
+let lightMode = false;
+
 window.addEventListener("load", windowLoadEvent => {
+    // click on light toggle --> switch light <--> dark mode
+    document.querySelectorAll(".brightness-toggle").forEach(elem => {
+        elem.addEventListener("click", brightnessToggleClickEvent => {
+            lightMode = !lightMode;
+            if (initialBrightness) {
+                document.querySelectorAll(".brightness-toggle").forEach(elem => {
+                    elem.classList.remove("brightness-toggle-initial");
+                });
+                initialBrightness = false;
+            }
+            document.body.classList.remove(lightMode ? "dark-mode": "light-mode");
+            document.body.classList.add(lightMode ? "light-mode": "dark-mode");
+        });
+    });
+
+    // click on email --> attempt email copy, update icon accordingly
     document.getElementById("email").addEventListener("click", emailClickEvent => {
         const email = emailClickEvent.target.innerText;
         const emailCopyIcon = document.getElementById("email-copy-icon");
