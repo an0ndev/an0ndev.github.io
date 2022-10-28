23d6ffdbd689edc36965f01050dc6bfddc721ed4
Eric Reed <mrericcoolnessreed@gmail.com>
2022-10-28 17:35:20 -0400

    Dev env: fix for sending files from subdirectories

--- local_dev.py
+++ local_dev.py
@@ -3,7 +3,7 @@ import pathlib
 import flask
 
 app = flask.Flask(__name__)
-containing_dir = pathlib.Path(__name__).parent
+containing_dir = pathlib.Path(__file__).parent
 
 @app.route("/", defaults = {"path_str": ""})
 @app.route("/<path:path_str>")
@@ -11,7 +11,7 @@ def all_requests(path_str):
     path = pathlib.Path(path_str)
     if path.is_dir(): path /= "index.html"
     # not worrying about security here (e.g. ".." in path_str, etc.) since this is for local development only
-    return flask.send_from_directory(containing_dir, path, as_attachment = False)
+    return flask.send_file(containing_dir.joinpath(path), as_attachment = False)
 
 if __name__ == "__main__":
     app.run(host = "0.0.0.0")
